a6f7faa0b8782e71c26951d3f8f4708989035c90
Lonami Exo <totufals@hotmail.com>
2018-05-30 11:25:22 +0200

    Add start method

--- lonabot/bot.py
+++ lonabot/bot.py
@@ -1,6 +1,16 @@
 import asyncio
+import re
+
+from constants import MAX_REMINDERS
 
 from dumbot.dumbaio import Bot
+
+
+def cmd(text):
+    def decorator(f):
+        f._trigger = text
+        return f
+    return decorator
 
 
 class Lonabot(Bot):
@@ -10,9 +20,20 @@ class Lonabot(Bot):
         self._running = False
         self._updates_loop = None
         self._last_id = 0
+        self._cmd = []
+        self.me = None
 
-    def start(self):
+    async def start(self):
         self._running = True
+        self.me = await self.getMe()
+        self._cmd.clear()
+        for m in dir(self):
+            m = getattr(self, m)
+            trigger = getattr(m, '_trigger', None)
+            if isinstance(trigger, str):
+                self._cmd.append((
+                    re.compile(f'{trigger}(@{self.me.username})?').match, m))
+
         self._updates_loop = asyncio.ensure_future(
             self._updates_loop_impl())
 
@@ -35,5 +56,39 @@ class Lonabot(Bot):
 
     # Actual methods we'll be using
     async def _on_update(self, update):
-        print(await self.sendMessage(chat_id=update.message.from_.id,
-                                     text=update.message.text[::-1]))
+        if not update.message.text:
+            return
+
+        for trigger, method in self._cmd:
+            if trigger(update.message.text):
+                await method(update)
+                return
+
+        await self.sendMessage(chat_id=update.message.from_.id,
+                               text='Say what?')
+
+    @cmd(r'/start')
+    async def _start(self, update):
+        await self.sendMessage(
+            chat_id=update.message.chat.id,
+            text=f'''
+Hi! I'm {self.me.first_name.title()} and running in "reminder" mode.
+
+You can set reminders by using:
+`/remindat 17:05 Optional text`
+`/remindin    5m Optional text`
+
+Or list those you have by using:
+`/status`
+
+Everyone is allowed to use {MAX_REMINDERS} reminders max. No more!
+
+Made with love by @Lonami and hosted by Richard ❤️
+'''.strip(), parse_mode='markdown')
+
+    @cmd(r'/(remindat|remindin|status|clear)')
+    async def _soon(self, update):
+        await self.sendMessage(
+            chat_id=update.message.chat.id,
+            text='Coming soon!'
+        )
---
+++ lonabot/constants.py
@@ -0,0 +1,1 @@
+MAX_REMINDERS = 10
