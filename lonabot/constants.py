48c1e07671d25746b01702ff1a093eca39a7cd29
Lonami <totufals@hotmail.com>
2019-01-17 23:20:33 +0100

    Round to the nearest 30 minutes

--- lonabot/constants.py
+++ lonabot/constants.py
@@ -1,2 +1,2 @@
 MAX_REMINDERS = 10  # Reminders per person
-MAX_TZ_STEP = 5  # How bad can someone's clock be when setting a timezone
+MAX_TZ_STEP = 30  # How bad can someone's clock be when setting a timezone
