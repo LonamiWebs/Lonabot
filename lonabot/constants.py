86b9e2c126934f139d39e2028f643e7885b8a65a
Lonami <totufals@hotmail.com>
2019-11-27 09:38:05 +0100

    Free reminders for everyone

--- lonabot/constants.py
+++ lonabot/constants.py
@@ -1,3 +1,3 @@
-MAX_REMINDERS = 10  # Reminders per person
-MAX_BIRTHDAYS = 10  # Birthdays per person
+MAX_REMINDERS = 100  # Reminders per person
+MAX_BIRTHDAYS = 30  # Birthdays per person
 MAX_TZ_STEP = 30  # How bad can someone's clock be when setting a timezone
